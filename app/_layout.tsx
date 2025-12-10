8ca900c802ff8aa10a6cdb4693eae924a4806582
Maximilian <maximilian.dregewsky@gmail.com>
2025-12-10 18:20:08 +0100

    Add theme.ts for global Colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,12 @@
 import { Slot, usePathname } from "expo-router";
 import React from "react";
-import { Text, View, Button } from 'react-native';
-import MenuBar from "./components/MenuBar";
-
 
 export default function Layout() {
     const path = usePathname();
 
-    // pages where menu bar should not be shown
-    const showMenu =
-        //!path.includes("/screens/LoginScreen") &&
-        !path.includes("/screens/RegisterScreen") &&
-        !path.includes("/screens/ClickerScreen");
-
     return (
         <>
             <Slot />
-
         </>
     );
 }
---
+++ app/theme.ts
@@ -0,0 +1,14 @@
+// theme.ts
+export const Colors = {
+    primary: "#AB8FFF",
+    background: "#121212",
+    iconActive: "#fff",
+    iconInactive: "#333",
+};
+
+export const DarkColors = {
+    primary: "#BB86FC",
+    background: "#121212",
+    iconActive: "#FFFFFF",
+    iconInactive: "#B0B0B0",
+};
