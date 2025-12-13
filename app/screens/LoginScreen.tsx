c6efb86071bf4a9e300eb91bcd393aef018ea393
Maximilian <maximilian.dregewsky@gmail.com>
2025-12-13 12:34:00 +0100

    Add KeyboardAvoidingView to LoginScreen.tsx and RegisterScreen.tsx

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "expo-router";
-import { Text, View, Pressable, TextInput,TouchableOpacity, Alert } from 'react-native';
+import { Text, View, Pressable, TextInput,TouchableOpacity, Alert, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform } from 'react-native';
 import {useState} from "react";
 import { auth } from "../firebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -52,97 +52,108 @@ export default function LoginScreen(){
     }
 
     return(
-        <View style={styles.container}>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"} // iOS verschiebt, Android passt Höhe an
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
 
-            {/* Title */}
-            <View style={styles.titleWrapper}>
-                <Text style={styles.title}>Willkommen bei</Text>
-                <Text style={styles.appname}>APPNAME!</Text>
-            </View>
+                    {/* Title */}
+                    <View style={styles.titleWrapper}>
+                        <Text style={styles.title}>Willkommen bei</Text>
+                        <Text style={styles.appname}>APPNAME!</Text>
+                    </View>
 
-            {/* Inputs */}
-            <View style={styles.inputs}>
+                    {/* Inputs */}
+                    <View style={styles.inputs}>
 
-                {/* E-Mail */}
-                <View style={styles.inputRow}>
-                    <Ionicons
-                        name="person-outline"
-                        size={28}
-                        color="#555"
-                        style={styles.icon}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="E-Mail"
-                        value={email}
-                        onChangeText={setEmail}
-                    />
+                        {/* E-Mail */}
+                        <View style={styles.inputRow}>
+                            <Ionicons
+                                name="person-outline"
+                                size={28}
+                                color="#555"
+                                style={styles.icon}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder="E-Mail"
+                                value={email}
+                                onChangeText={setEmail}
+                            />
+                        </View>
+
+                        {/* Password */}
+                        <View style={styles.inputRow}>
+                            <Ionicons
+                                name="lock-closed-outline"
+                                size={28}
+                                color="#555"
+                                style={styles.icon}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Passwort"
+                                value={password}
+                                onChangeText={setPassword}
+                                secureTextEntry={hidden}/>
+
+                            <TouchableOpacity onPress={() => setHidden(!hidden)}>
+                                <Ionicons
+                                    name={hidden ? "eye-outline" : "eye-off-outline"}
+                                    size={24}
+                                    color="#555"
+                                    style={styles.eyeIcon}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    {/* Login */}
+                    <View style={styles.buttonWrapper}>
+                        <Pressable
+                            onPress={handleLogin}
+                            style={({ pressed }) => [
+                                styles.button,
+                                {backgroundColor: pressed ? Colors.secondary : Colors.primary},
+                                {borderColor: pressed ? Colors.secondary : Colors.primary}
+                            ]}
+                        >
+                            <Text style={styles.buttonText}>Einloggen</Text>
+                        </Pressable>
+                    </View>
+
+                    {/* Noch kein Konto */}
+                    <View style={{marginTop: 40,}}>
+                        <View style={{flexDirection:"row",justifyContent:"space-around",alignItems: "center"}}>
+                            <View style={styles.line}/>
+                            <Text style={styles.smallText}>Noch kein Konto?</Text>
+                            <View style={styles.line}/>
+                        </View>
+
+                        {/* to RegisterScreen */}
+                        <Pressable
+                            onPress={() => router.replace("/screens/RegisterScreen")}
+                            style={({ pressed }) => [
+                                styles.secondaryBotton,
+                                {backgroundColor: pressed ? "#eee" : 'transparent'},
+                                {borderColor: pressed ? Colors.secondary : Colors.primary}
+                            ]}
+                        >
+                            <Text style={styles.secondaryButtonText}>Konto erstellen</Text>
+                        </Pressable>
+                    </View>
+
+                    {/* Loading Overlay */}
+                    <LoadingOverlay visible={loading} />
+
                 </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
 
-                {/* Password */}
-                <View style={styles.inputRow}>
-                    <Ionicons
-                        name="lock-closed-outline"
-                        size={28}
-                        color="#555"
-                        style={styles.icon}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Passwort"
-                        value={password}
-                        onChangeText={setPassword}
-                        secureTextEntry={hidden}/>
 
-                    <TouchableOpacity onPress={() => setHidden(!hidden)}>
-                        <Ionicons
-                            name={hidden ? "eye-outline" : "eye-off-outline"}
-                            size={24}
-                            color="#555"
-                            style={styles.eyeIcon}
-                        />
-                    </TouchableOpacity>
-                </View>
-            </View>
 
-            {/* Login */}
-            <View style={styles.buttonWrapper}>
-                <Pressable
-                    onPress={handleLogin}
-                    style={({ pressed }) => [
-                        styles.button,
-                        {backgroundColor: pressed ? Colors.secondary : Colors.primary},
-                        {borderColor: pressed ? Colors.secondary : Colors.primary}
-                    ]}
-                >
-                    <Text style={styles.buttonText}>Einloggen</Text>
-                </Pressable>
-            </View>
 
-            {/* Noch kein Konto */}
-            <View style={{marginTop: 40,}}>
-                <View style={{flexDirection:"row",justifyContent:"space-around",alignItems: "center"}}>
-                    <View style={styles.line}/>
-                    <Text style={styles.smallText}>Noch kein Konto?</Text>
-                    <View style={styles.line}/>
-                </View>
-
-                {/* to RegisterScreen */}
-                <Pressable
-                    onPress={() => router.replace("/screens/RegisterScreen")}
-                    style={({ pressed }) => [
-                        styles.secondaryBotton,
-                        {backgroundColor: pressed ? "#eee" : 'transparent'},
-                        {borderColor: pressed ? Colors.secondary : Colors.primary}
-                    ]}
-                >
-                    <Text style={styles.secondaryButtonText}>Konto erstellen</Text>
-                </Pressable>
-            </View>
-
-            {/* Loading Overlay */}
-            <LoadingOverlay visible={loading} />
-
-        </View>
     );
 }
